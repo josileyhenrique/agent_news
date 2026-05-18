5dadd532a3380279d93ce8ee25206ffa225bc3a9
josileyhenrique <josileyhenrique17@gmail.com>
2026-05-18 19:35:47 -0300

    Corrigindo versão do gemini gratuito

--- agentnews.py
+++ agentnews.py
@@ -37,7 +37,7 @@ def formatar_com_gemini(titulo_noticia):
     )
     
     response = client.models.generate_content(
-        model="gemini-1.5-flash", 
+        model="gemini-2.5-flash", 
         contents=prompt
     )
     return response.text
